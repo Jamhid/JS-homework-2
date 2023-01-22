ef8173fe6f3534933013103dac80f8b9c93894fd
Jama <jamshid.abdumusayev@mail.ru>
2023-01-22 13:44:07 +0500

    JS homework-2

---
+++ script.js
@@ -0,0 +1,149 @@
+// var age = +prompt('Введите свой возраст');
+
+//     if (age <= 18) {
+//         console.log('Вы ещё молоды, Вам нужно учиться');
+//         alert('Вы ещё молоды, Вам нужно учиться')
+//     }else if (age <= 50) {
+//         console.log(' Вам нужно работать ');
+//         alert('Вам нужно работать')
+//     }else if (age <= 59) {
+//         console.log(' Вам скоро на пенсию ');
+//         alert('Вам скоро на пенсию')
+//     }else if (age <= 100){
+//         console.log(' Вы пенсионер ');
+//         alert('Вы пенсионер')
+//     }else  {
+//         console.log(' Что-то пошло не так ' );
+//         alert('Что-то пошло не так')
+//     }
+    
+    
+// var num = +prompt('Время');
+
+//     switch (num) {
+//         case 1:
+//             console.log( num + ' часа ночи ' );
+//             alert(num + ' часа ночи ')
+//             break;
+//         case 2:
+//             console.log( num + ' часа ночи ' );
+//             alert(num + ' часа ночи ')
+//             break;
+//         case 3:
+//             console.log( num + ' часа ночи ' );
+//             alert(num + ' часа ночи ')
+//             break;
+//         case 4:
+//             console.log( num + ' часа ночи ' );
+//             alert(num + ' часа ночи ')
+//             break;
+//         case 5:
+//             console.log( num + ' часа ночи ' );
+//             alert(num + ' часа ночи ')
+//             break;
+//         case 6:
+//             console.log( num + ' часа ночи ' );
+//             alert(num + ' часа ночи ')
+//             break;
+//         case 7:
+//             console.log( num + ' часов утра ' );
+//             alert(num + ' часов утра ')
+//             break;
+//         case 8:
+//             console.log( num + ' часов утра ' );
+//             alert(num + ' часов утра ')
+//             break;
+//         case 9:
+//             console.log( num + ' часов утра ' );
+//             alert(num + ' часов утра ')
+//             break;
+//         case 10:
+//             console.log( num + ' часов утра ' );
+//             alert(num + ' часов утра ')
+//             break;
+//         case 11:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 12:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 13:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 14:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 15:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 16:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 17:
+//             console.log( num + ' часа дня ' );
+//             alert(num + ' часа дня ')
+//             break;
+//         case 18:
+//             console.log( num + ' часов вечера ' );
+//             alert(num + ' часов вечера ')
+//             break;
+//         case 19:
+//             console.log( num + ' часов вечера ' );
+//             alert(num + ' часов вечера ')
+//             break;
+//         case 20:
+//             console.log( num + ' часов вечера ' );
+//             alert(num + ' часов вечера ')
+//             break;
+//         case 21:
+//             console.log( num + ' часов вечера ' );
+//             alert(num + ' часов вечера ')
+//             break;
+//         case 22:
+//             console.log( num + ' часов вечера ' );
+//             alert(num + ' часов вечера ')
+//             break;
+//         case 23:
+//             console.log( num + ' часов вечера ' );
+//             alert(num + ' часов вечера ')
+//             break;
+    
+//         default:
+//             console.log('Что-то пошло не так!');
+//             alert('Что-то пошло не так!')
+//             break;
+//     }   
+
+
+var a = +prompt('Любое число');
+var b = +prompt('Любое число');
+var c = +prompt('Любое число');
+
+    if (a < b < c) {
+        console.log(b + ' Среднее число ');
+        alert(b + ' Среднее число ')
+    }else if (c < b < a) {
+        console.log(b + ' Среднее число ');
+        alert(b + ' Среднее число ')
+    }else {
+        if (b < c < a) {
+            console.log(c + ' Среднее число ');
+            alert(c + ' Среднее число ')
+    }else if (a < c < b) {
+            console.log(c + ' Среднее число ');
+            alert(c + ' Среднее число ')
+    }else {
+        if (b < a < c) {
+            console.log(a + ' Среднее число ');
+            alert(a + ' Среднее число ')
+        }else if (c < a < b) {
+            console.log(a + ' Среднее число ');
+            alert(a + ' Среднее число ')
+        }}
+    }
